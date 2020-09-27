afa96b65887ebe04de2cfa9e0724dc028be29cde
Gompa27 <campillo.gonzalo@gmail.com>
2020-09-27 20:22:59 -0300

    Nodos correcion

--- Node-Defense/Assets/Scripts/LevelManager.cs
+++ Node-Defense/Assets/Scripts/LevelManager.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager1 : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
 
     public BinaryTree nodes;
     private const int NODE_POSITION_DIFERENCESS = 2;
+    public Transform nodesParent;
 
     public Dictionary<string, string> dictionary;
 
     public List<GameObject> nodesPrefabs;
     
-    static public GameManager1 instance;
+    static public LevelManager instance;
 
     void Awake()
     {
         nodes = new BinaryTree();
         int i = 0;
-        var internet = Instantiate(nodesPrefabs[1]);
+        var internet = Instantiate(nodesPrefabs[1], nodesParent);
         Vector2 position = new Vector2(-6, 0);
         ((GameObject)internet).transform.localPosition = position;
         nodes.Add(i, internet.GetComponent<GameNode>());
@@ -26,14 +27,14 @@ public class GameManager1 : MonoBehaviour
         {
             position.x += NODE_POSITION_DIFERENCESS;
             position.y = Random.Range(-5, 5);
-            var basicNode = Instantiate(nodesPrefabs[0]);
+            var basicNode = Instantiate(nodesPrefabs[0], nodesParent);
             ((GameObject)basicNode).transform.localPosition = position;
             nodes.Add(i, internet.GetComponent<GameNode>());
         }
         i++;
         position.x += NODE_POSITION_DIFERENCESS;
         position.y = 0;
-        var server = Instantiate(nodesPrefabs[2]);
+        var server = Instantiate(nodesPrefabs[2], nodesParent);
         ((GameObject)server).transform.localPosition = position;
         nodes.Add(i, internet.GetComponent<GameNode>());
 
