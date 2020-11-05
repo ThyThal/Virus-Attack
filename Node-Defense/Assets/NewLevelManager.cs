601d3a2f8d973181e275eb4711e435f7c7bc5695
Alejo Gaitán <thy.gaitan@gmail.com>
2020-11-05 20:50:36 -0300

    Working nodeManager

---
+++ Node-Defense/Assets/NewLevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewLevelManager : MonoBehaviour
+{
+    [SerializeField] public NewLevelManager Instance;
+    [SerializeField] private Transform nodesParent;
+    [SerializeField] private List<GameObject> nodesPrefabs;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+
+    private void CreateInternet()
+    {
+
+    }
+
+    private void CreateNode()
+    {
+
+    }
+
+    private void CreateServer()
+    {
+
+    }
+}
--- Node-Defense/Assets/Scripts/Managers/NodeManager.cs
+++ Node-Defense/Assets/Scripts/Managers/NodeManager.cs
@@ -23,23 +23,43 @@ public class NodeManager : MonoBehaviour
     [SerializeField] private GameObject nodeBasic;
     [SerializeField] private GameObject nodeServer;
 
+    [Header("Nodes Position")]
+    [SerializeField] private Transform nodesParent;
+    [SerializeField] private Vector2 position;
+    [SerializeField] private float spacing;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
     private void Awake()
     {
         instance = this;
 
+        // Creamos la cantidad de vertices a inicializar.
         for (int i = 0; i < Random.Range(minRandom, maxRandom); i++)
         {
             vertexInit.Add(i + 1);
         }
 
+        // Inicializamos los vertices.
         vertex = new int[vertexInit.Count];
-        for (int i = 0; i < vertexInit.Count; i++)
+        
+        // Instanciar Nodo Internet.
+        CreateInternet(position);
+        position.x = position.x + spacing; // Agregar Espacio Entre Nodos.
+
+        // Instanciar Nodos Basicos.
+        for (int i = 1; i < vertexInit.Count-1; i++)
         {
             vertex[i] = vertexInit[i];
+            CreateNode(position);
+            position.x = position.x + spacing; // Agregar Espacio Entre Nodos.
         }
 
-        int currentVertex = vertexInit[0];
+        // Instanciar Nodo Servidor.
+        CreateServer(position);
+
         int lastVertex = vertexInit[vertexInit.Count - 1]; // Ultimo vertice que va a tener conexiones.
+        int currentVertex = vertexInit[0];
         vertexInit.Remove(currentVertex); // Remueve primer nodo.
         vertexInit.Remove(lastVertex); // Remueve ultimo nodo antes del servidor.
 
@@ -54,8 +74,11 @@ public class NodeManager : MonoBehaviour
                 edges.Add(new Vector3(currentVertex, vertexInit[index], 1)); // From, To, Weight.
                 if (vertexInit.Count <= 1) break;
                 int aux1 = index;
+
                 while (index == aux1)
+                {
                     index = Random.Range(0, vertexInit.Count);
+                }
 
             }
             int aux = currentVertex;
@@ -64,5 +87,34 @@ public class NodeManager : MonoBehaviour
         }
 
         edges.Add(new Vector3(currentVertex, lastVertex, 1));
+    }
+
+    private void CreateInternet(Vector2 p)
+    {
+        var node = Instantiate(nodeInternet, nodesParent);
+        var nodeTransform = node.transform;
+        RandomizePosition(nodeTransform, position);
+    }
+
+    private void CreateNode(Vector2 p)
+    {
+        var node = Instantiate(nodeBasic, nodesParent);
+        var nodeTransform = node.transform;
+        RandomizePosition(nodeTransform, position);
+    }
+
+    private void CreateServer(Vector2 p)
+    {
+        var node = Instantiate(nodeServer, nodesParent);
+        var nodeTransform = node.transform;
+        RandomizePosition(nodeTransform, position);
+
+    }
+
+    private Vector2 RandomizePosition(Transform t, Vector2 p)
+    {
+        t.position = new Vector2(p.x, Random.Range(minY, maxY));
+        p.x = p.x + spacing;
+        return p;
     }
 }
