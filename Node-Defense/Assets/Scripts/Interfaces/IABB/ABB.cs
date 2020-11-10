3e879931b9f8ffa75c69638ab3866fb05a6e3c48
Gompa27 <campillo.gonzalo@gmail.com>
2020-11-10 19:11:24 -0300

    ABB

---
+++ Node-Defense/Assets/Scripts/Interfaces/IABB/ABB.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABB : IABB
+{
+    public NodoABB raiz;
+
+    public int Raiz()
+    {
+        return raiz.info;
+    }
+
+    public bool ArbolVacio()
+    {
+        return (raiz == null);
+    }
+
+    public void InicializarArbol()
+    {
+        raiz = null;
+    }
+
+    public NodoABB HijoDer()
+    {
+        return raiz.hijoDer;
+    }
+
+    public NodoABB HijoIzq()
+    {
+        return raiz.hijoIzq;
+    }
+
+    public void AgregarElem(ref NodoABB raiz, int x)
+    {
+        if (raiz == null)
+        {
+            raiz = new NodoABB();
+            raiz.info = x;
+        }
+        else if (raiz.info > x)
+        {
+            AgregarElem(ref raiz.hijoIzq, x);
+        }
+        else if (raiz.info < x)
+        {
+            AgregarElem(ref raiz.hijoDer, x);
+        }
+    }
+
+    public void EliminarElem(ref NodoABB raiz, int x)
+    {
+        if (raiz != null)
+        {
+            if (raiz.info == x && (raiz.hijoIzq == null) && (raiz.hijoDer == null))
+            {
+                raiz = null;
+            }
+            else if (raiz.info == x && raiz.hijoIzq != null)
+            {
+                raiz.info = this.mayor(raiz.hijoIzq);
+                EliminarElem(ref raiz.hijoIzq, raiz.info);
+            }
+            else if (raiz.info == x && raiz.hijoIzq == null)
+            {
+                raiz.info = this.menor(raiz.hijoDer);
+                EliminarElem(ref raiz.hijoDer, raiz.info);
+            }
+            else if (raiz.info < x)
+            {
+                EliminarElem(ref raiz.hijoDer, x);
+            }
+            else
+            {
+                EliminarElem(ref raiz.hijoIzq, x);
+            }
+        }
+    }
+
+    public int mayor(NodoABB a)
+    {
+        if (a.hijoDer == null)
+        {
+            return a.info;
+        }
+        else
+        {
+            return mayor(a.hijoDer);
+        }
+    }
+
+    public int menor(NodoABB a)
+    {
+        if (a.hijoIzq == null)
+        {
+            return a.info;
+        }
+        else
+        {
+            return menor(a.hijoIzq);
+        }
+    }
+
+}
---
+++ Node-Defense/Assets/Scripts/Interfaces/IABB/NodoABB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodoABB
+{
+    // datos a almacenar, en este caso un entero
+    public int info;
+    // referencia los nodos izquiero y derecho
+    public NodoABB hijoIzq = null;
+    public NodoABB hijoDer = null;
+}
