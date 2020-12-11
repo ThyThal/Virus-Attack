aa2ae80c74882da1f72a167f651462088c98032b
Alejo Gaitán <thy.gaitan@gmail.com>
2020-12-11 15:24:00 -0300

    Line Renderer Fixed

---
+++ Node-Defense/Assets/GridLineRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridLineRenderer : MonoBehaviour
+{
+    [Header("Grid Info.")]
+    [SerializeField] private Transform _gridTransform;
+    [SerializeField] private Vector3 _gridPosition;
+
+    [Header("Line Renderer Info.")]
+    [SerializeField] private GameObject _lineRendererHolder;
+    [SerializeField] private GameObject _lineEdge;
+    [SerializeField] private float _lineRendererScale;
+
+
+    private void Awake()
+    {
+        _gridPosition = Camera.main.ScreenToWorldPoint(_gridTransform.position);
+        InstantiateHolder();
+    }
+
+    private void InstantiateHolder()
+    {
+        var nodeEdge = Instantiate(_lineRendererHolder, this.transform);
+        nodeEdge.transform.position = _gridPosition;
+    }
+
+}
--- Node-Defense/Assets/Scripts/Managers/NodeManager.cs
+++ Node-Defense/Assets/Scripts/Managers/NodeManager.cs
@@ -6,19 +6,25 @@ using UnityEngine;
 public class NodeManager : MonoBehaviour
 {
     static public NodeManager Instance;
+    private List<int> vertexInit = new List<int>();
+    private List<int> visited = new List<int>();
 
-    [SerializeField] private List<int> vertexInit = new List<int>();
-    [SerializeField] public int[] vertex;
-    [SerializeField] private List<int> visited;
-    [SerializeField] public List<Vector3> edges;
+    [Header("Public Node Info")]
+    public List<Vector3> edges;
+    public int[] vertex;
 
-    [Header("Nodes Info")]
-    [SerializeField] private List<Transform> transforms;
-    [SerializeField] private int minRandom;
-    [SerializeField] private int maxRandom;
-    [SerializeField] private int minEdges;
-    [SerializeField] private int maxEdges;
-    [SerializeField] private int edgeAmount;
+    [Header("Node Manager Info")]
+    [SerializeField] private int _minNodes;
+    [SerializeField] private int _maxNodes;
+    [SerializeField] private int _minEdges;
+    [SerializeField] private int _maxEdges;
+    private int _edgeAmount;
+
+    [Header("Line Renderer Info")]
+    [SerializeField] private GameObject _foundOrigin;
+    [SerializeField] private GameObject _foundDestiny;
+    [SerializeField] private float _scale;
+    [SerializeField] private GameObject lineEdgePrefab;
 
     [Header("Nodes")]
     [SerializeField] private GameObject nodeInternet;
@@ -28,16 +34,6 @@ public class NodeManager : MonoBehaviour
     [SerializeField] private GameObject nodeBasic;
     [SerializeField] private Transform nodeBasicPosition;
 
-    [Header("Nodes Position")]
-    [SerializeField] private float distanceY;
-    [SerializeField] private List<Vector2> currentXnodes;
-    [SerializeField] private Vector2 position;
-    [SerializeField] private float spacing;
-    [SerializeField] private float minY;
-    [SerializeField] private float maxY;
-    [SerializeField] private int nodesY;
-    [SerializeField] private float scale;
-
     [Header("Transforms")]
     [SerializeField] private List<Transform> transforms1;
     [SerializeField] private List<Transform> transforms2;
@@ -45,10 +41,10 @@ public class NodeManager : MonoBehaviour
     [SerializeField] private List<Transform> transforms4;
     [SerializeField] private List<Transform> transforms5;
     [SerializeField] private List<Transform> transforms6;
-    [SerializeField] private List<Transform> returnTransforms;
+    private List<Transform> returnTransforms;
 
 
-    public GameObject lineEdgePrefab;
+    
 
     public Dictionary<int, GameNode> nodesDictionary;
 
@@ -102,18 +98,18 @@ public class NodeManager : MonoBehaviour
         {            
             int index = Random.Range(0, vertexInit.Count); // Seleccionar un nodo aleatorio para conectarlo.
 
-            if (vertexInit.Count <= maxEdges) 
+            if (vertexInit.Count <= _maxEdges) 
             {
-                edgeAmount = Random.Range(minEdges, vertexInit.Count);
+                _edgeAmount = Random.Range(_minEdges, vertexInit.Count);
             } // Generamos la cantidad de conexiones a generar en index.
 
             else
             {
-                edgeAmount = Random.Range(minEdges, maxEdges);
+                _edgeAmount = Random.Range(_minEdges, _maxEdges);
             } // Si el maximo de conexiones es mayor que los items, se genera lo mayor posible.
 
             // Agregar conexion de "currentVertex" con el nodo "index".
-            for (int i = 0; i < edgeAmount; i++)
+            for (int i = 0; i < _edgeAmount; i++)
             {  
 
                 // Recorremos todos los nodos visitados para no volver atras.
@@ -177,13 +173,14 @@ public class NodeManager : MonoBehaviour
     private void CreateInitialNodes()
     {
         // Creamos la cantidad de vertices a inicializar.
-        for (int i = 0; i < Random.Range(minRandom, maxRandom); i++)
+        for (int i = 0; i < Random.Range(_minNodes, _maxNodes); i++)
         {
             vertexInit.Add(i + 1);
         }
 
         vertex = new int[vertexInit.Count]; // Inicializamos los vertices.
     }
+
     private void CreateNodeInternet()
     {
         vertex[0] = vertexInit[0];
@@ -191,6 +188,8 @@ public class NodeManager : MonoBehaviour
         var gameNode = node.GetComponent<GameNode>();
         gameNode.Vertex = vertex.First();
         nodesDictionary.Add(vertex.First(), gameNode);
+
+        
     }
     private void CreateNodeServer()
     {
@@ -199,6 +198,7 @@ public class NodeManager : MonoBehaviour
         var gameNode = node.GetComponent<GameNode>();
         gameNode.Vertex = vertex.Last();
         nodesDictionary.Add(vertex.Last(), gameNode);
+
     }
     private void CreateBasicNode(int id, Transform nodeBasicPosition)
     {
@@ -206,7 +206,9 @@ public class NodeManager : MonoBehaviour
         var gameNode = node.GetComponent<GameNode>();
         gameNode.Vertex = id;
         nodesDictionary.Add(id, gameNode);
+
     }
+
     private void InstantiateEdge(int origin, int destiny)
     {
         GameNode currentNode;
@@ -214,20 +216,25 @@ public class NodeManager : MonoBehaviour
         nodesDictionary.TryGetValue(origin, out currentNode);
         nodesDictionary.TryGetValue(destiny, out destinyNode);
 
-        var lineEdge = Instantiate(lineEdgePrefab, currentNode.gameObject.transform); // Prefab del LineRenderer.
+        FindNodeObject(currentNode, destinyNode);
+        var lineEdge = Instantiate(lineEdgePrefab, _foundOrigin.transform); // Prefab del LineRenderer.
         lineEdge.GetComponent<LineRenderer>().SetPosition(0, Vector3.zero); // Posicion de Origen.
 
+        Vector3 vectorToTarget = (_foundDestiny.transform.position - _foundOrigin.transform.position) * _scale; // FALLA
 
-
-        Vector3 vectorToTarget = (destinyNode.transform.localPosition - currentNode.transform.localPosition) * scale; // FALLA
-
-        if (vectorToTarget == Vector3.zero) { 
-            vectorToTarget = (Vector3.one) * scale; }
+        if (vectorToTarget == Vector3.zero) {
+            Debug.Log(vectorToTarget); }
         
-
-
-
         lineEdge.GetComponent<LineRenderer>().SetPosition(1, vectorToTarget);
         currentNode.edgesRenderers.Add(lineEdge);
+    }
+
+    private void FindNodeObject(GameNode originEdgeObject, GameNode destinyEdgeObject)
+    {
+        var originString = originEdgeObject.transform.parent.name;
+        _foundOrigin = GameObject.Find(originString);
+
+        var destinyString = destinyEdgeObject.transform.parent.name;
+        _foundDestiny = GameObject.Find(destinyString);
     }
 }
